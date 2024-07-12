8eef73459c3e60588209864142a191ce0a2d95bd
eucleiance <eucleiance@gmail.com>
2024-07-12 23:32:58 +0545

    Functions, Events, etc

--- huesmith/extension.js
+++ huesmith/extension.js
@@ -6,68 +6,71 @@ function activate(context) {
   // Register the command
   const disposable = vscode.commands.registerCommand('huesmith.updateColor', function () {
     console.log('Command huesmith.updateColor executed.');
-    updateColorConfiguration();
+    update('PrimaryBackground');
   });
 
   context.subscriptions.push(disposable);
 
   // Watch for configuration changes
   vscode.workspace.onDidChangeConfiguration(event => {
-    if (event.affectsConfiguration('PrimaryBackground.HexCode')) {
-      console.log('PrimaryBackground configuration changed.');
-      updateColorConfiguration();
+    if (event.affectsConfiguration('PrimaryBackground.HexCode') || event.affectsConfiguration('PrimaryBackground.Enable')) {
+      console.log('PrimaryBackground Settings Changed');
+      update('PrimaryBackground');
     }
   });
 
   // Initial update
-  updateColorConfiguration();
+  update('PrimaryBackground');
 }
 
-function updateColorConfiguration() {
-  const config = vscode.workspace.getConfiguration('PrimaryBackground');
-  const primaryBackground = config.get('HexCode');
-
-  console.log('PrimaryBackground value:', primaryBackground);
-
-  // Get the current workbench color customizations
-  const currentColorCustomizations = vscode.workspace.getConfiguration('workbench').get('colorCustomizations') || {};
-
-  console.log('Current workbench.colorCustomizations:', currentColorCustomizations);
-
-  // Update the color customizations with the new color
-  const updatedColorCustomizations = {
-    ...currentColorCustomizations,
-    "activityBar.background": primaryBackground,
-    "sideBar.background": primaryBackground,
-    "sideBarSectionHeader.background": primaryBackground,
-    "statusBar.background": primaryBackground,
-    "titleBar.activeBackground": primaryBackground,
-    "titleBar.inactiveBackground": primaryBackground,
-    "editor.background": primaryBackground,
-    "editorGroupHeader.tabsBackground": primaryBackground,
-    "editorGutter.background": primaryBackground,
-    "tab.inactiveBackground": primaryBackground,
-    "tab.unfocusedInactiveBackground": primaryBackground,
-    "breadcrumb.background": primaryBackground,
-    "panel.background": primaryBackground,
-    "terminal.background": primaryBackground,
-  };
-
-  console.log('Updated workbench.colorCustomizations:', updatedColorCustomizations);
-
-  //
-// Update the settings with the new color customizations
-vscode.workspace.getConfiguration('workbench').update('colorCustomizations', updatedColorCustomizations, vscode.ConfigurationTarget.Global);
-  // // Update the settings with the new color customizations
-  // vscode.workspace.getConfiguration('workbench').update('colorCustomizations', updatedColorCustomizations, vscode.ConfigurationTarget.Global)
-  //   .then(() => {
-  //     console.log('Updated workbench.colorCustomizations in settings.json');
-  //   }, error => {
-  //     console.error('Failed to update workbench.colorCustomizations:', error);
-  //   });
+function status(category) {
+  const enableStatus = vscode.workspace.getConfiguration(category).get('Enable');
+  console.log(`Status of ${category}:`, enableStatus);
+  return enableStatus;
 }
 
-function deactivate() {}
+function update(category) {
+  try {
+    const inputHex = vscode.workspace.getConfiguration(category).get('HexCode');
+    const currentConfig = vscode.workspace.getConfiguration('workbench').get('colorCustomizations') || {};
+    const updatedConfig = updateTheme(category, status(category), inputHex, currentConfig);
+
+    vscode.workspace.getConfiguration('workbench').update('colorCustomizations', updatedConfig, vscode.ConfigurationTarget.Global);
+  } catch (error) {
+    console.error(`Failed to update color configuration for ${category}:`, error);
+  }
+}
+
+function updateTheme(category, isEnabled, HexCode, currentColorCustomizations) {
+  if (category === 'PrimaryBackground' && isEnabled === true) {
+    console.log('Updating theme with:', {
+      category,
+      isEnabled,
+      HexCode,
+      currentColorCustomizations
+    });
+    return {
+      ...currentColorCustomizations,
+      "activityBar.background": HexCode,
+      "sideBar.background": HexCode,
+      "sideBarSectionHeader.background": HexCode,
+      "statusBar.background": HexCode,
+      "titleBar.activeBackground": HexCode,
+      "titleBar.inactiveBackground": HexCode,
+      "editor.background": HexCode,
+      "editorGroupHeader.tabsBackground": HexCode,
+      "editorGutter.background": HexCode,
+      "tab.inactiveBackground": HexCode,
+      "tab.unfocusedInactiveBackground": HexCode,
+      "breadcrumb.background": HexCode,
+      "panel.background": HexCode,
+      "terminal.background": HexCode,
+    };
+  }
+  return currentColorCustomizations;
+}
+
+function deactivate() { }
 
 module.exports = {
   activate,
