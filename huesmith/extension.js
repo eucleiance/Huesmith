7bae84c0ccfd4334a043830144c33691f7a2823d
eucleiance <eucleiance@gmail.com>
2024-07-13 18:43:05 +0545

    pushing before i f.up trying to implement semantic highlighting

--- huesmith/extension.js
+++ huesmith/extension.js
@@ -6,27 +6,33 @@ function activate(context) {
   // Register the command
   const disposable = vscode.commands.registerCommand('huesmith.updateColor', function () {
     console.log('Command huesmith.updateColor executed.');
-    update('PrimaryBackground');
+    // update('PrimaryBackground');
+    // console.log('Notificasionnnnnn...')
+    vscode.window.showInformationMessage('Notificasionnnn....')
   });
 
   context.subscriptions.push(disposable);
 
   // Watch for configuration changes
-  vscode.workspace.onDidChangeConfiguration(event => {
-    if (event.affectsConfiguration('PrimaryBackground.HexCode') || event.affectsConfiguration('PrimaryBackground.Enable')) {
-      console.log('PrimaryBackground Settings Changed');
-      update('PrimaryBackground');
-    }
+  // Register configuration change listeners
+  const categories = ['PrimaryBackground', 'SecondaryBackground', 'PrimaryForeground', 'SecondaryForeground', 'Borders'];
+  categories.forEach(category => {
+    registerConfigurationListener(category);
   });
-
-  vscode.workspace.onDidChangeConfiguration(event => {
-    if (event.affectsConfiguration('PrimaryForeground.HexCode') || event.affectsConfiguration('PrimaryForeground.Enable')) {
-      console.log('Primaryforeground Settings Changed');
-      update('PrimaryForeground');
-    }
-  });
-
 }
+
+function registerConfigurationListener(category) {
+  vscode.workspace.onDidChangeConfiguration(event => {
+    const hexCodeSetting = `${category}.HexCode`;
+    const enableSetting = `${category}.Enable`;
+
+    if (event.affectsConfiguration(hexCodeSetting) || event.affectsConfiguration(enableSetting)) {
+      console.log(`${category} Settings Changed`);
+      update(category);
+    }
+  });
+}
+
 
 function status(category) {
   const enableStatus = vscode.workspace.getConfiguration(category).get('Enable');
@@ -38,7 +44,9 @@ function update(category) {
   try {
     const inputHex = vscode.workspace.getConfiguration(category).get('HexCode');
     const currentConfig = vscode.workspace.getConfiguration('workbench').get('colorCustomizations') || {};
-    const updatedConfig = updateTheme(category, status(category), inputHex, currentConfig);
+    const updatedConfig = updateTheme(category, status(category), inputHex, { ...currentConfig }); // Ensure a copy is made
+
+    console.log('Updating configuration:', updatedConfig);
 
     vscode.workspace.getConfiguration('workbench').update('colorCustomizations', updatedConfig, vscode.ConfigurationTarget.Global);
   } catch (error) {
@@ -47,49 +55,102 @@ function update(category) {
 }
 
 function updateTheme(category, isEnabled, HexCode, currentColorCustomizations) {
-  if (category === 'PrimaryBackground' && isEnabled === true) {
-    console.log('Updating theme with:', {
-      category,
-      isEnabled,
-      HexCode,
-      currentColorCustomizations
-    });
-    return {
-      ...currentColorCustomizations,
-      "activityBar.background": HexCode,
-      "sideBar.background": HexCode,
-      "sideBarSectionHeader.background": HexCode,
-      "statusBar.background": HexCode,
-      "titleBar.activeBackground": HexCode,
-      "titleBar.inactiveBackground": HexCode,
-      "editor.background": HexCode,
-      "editorGroupHeader.tabsBackground": HexCode,
-      "editorGutter.background": HexCode,
-      "tab.inactiveBackground": HexCode,
-      "tab.unfocusedInactiveBackground": HexCode,
-      "breadcrumb.background": HexCode,
-      "panel.background": HexCode,
-      "terminal.background": HexCode,
-    };
-  } else if (category === 'PrimaryForeground' && isEnabled === true) {
-    console.log('Updating theme with:', {
-      category,
-      isEnabled,
-      HexCode,
-      currentColorCustomizations
-    });
-    return {
-      ...currentColorCustomizations,
-      "activityBar.inactiveForeground": HexCode,
-      "sideBar.foreground": HexCode,
-      "titleBar.activeForeground": HexCode,
-      "titleBar.inactiveForeground": HexCode,
-      "tab.inactiveForeground": HexCode,
-      "tab.unfocusedInactiveForeground": HexCode,
-    };
+  if (keysToUpdate[category]) {
+    keysToUpdate[category].forEach(key => {
+      if (isEnabled) {
+        currentColorCustomizations[key] = HexCode;
+      }
+      if (!isEnabled) {
+        delete currentColorCustomizations[key];
+      }
+    })
   }
+  // console.log('Current color customizations after update:', currentColorCustomizations);
   return currentColorCustomizations;
 }
+
+const keysToUpdate = {
+  PrimaryBackground: [
+    "activityBar.background",
+    "sideBar.background",
+    "sideBarSectionHeader.background",
+    "statusBar.background",
+    "titleBar.activeBackground",
+    "titleBar.inactiveBackground",
+    "editor.background",
+    "editorGroupHeader.tabsBackground",
+    "editorGutter.background",
+    "tab.inactiveBackground",
+    "tab.unfocusedInactiveBackground",
+    "breadcrumb.background",
+    "panel.background",
+    "terminal.background",
+    "minimap.background",
+  ],
+  SecondaryBackground: [
+    "activityBar.activeBackground",
+    "list.activeSelectionBackground",
+    "notifications.background",
+    "list.hoverBackground",
+    "list.focusBackground",
+    "input.background",
+    "editorWidget.background",
+    "tab.activeBackground",
+    "menu.background",
+  ],
+  PrimaryForeground: [
+    "list.hoverForeground",
+    "list.focusForeground",
+    "activityBar.foreground",
+    "sideBarSectionHeader.foreground",
+    "sideBarTitle.foreground",
+    "list.inactiveSelectionForeground",
+    "list.activeSelectionForeground",
+    "list.focusForeground",
+    "statusBar.foreground",
+    "tab.activeForeground",
+    "tab.unfocusedActiveForeground",
+  ],
+  SecondaryForeground: [
+    "activityBar.inactiveForeground",
+    "sideBar.foreground",
+    "titleBar.activeForeground",
+    "titleBar.inactiveForeground",
+    "tab.inactiveForeground",
+    "tab.unfocusedInactiveForeground",
+  ],
+  Borders: [
+    "focusBorder",
+    "activityBar.border",
+    "sideBar.border",
+    "sideBarSectionHeader.border",
+    "statusBar.border",
+    "statusBar.noFolderBorder",
+    "statusBar.debuggingBorder",
+    "statusBarItem.focusBorder",
+    "statusBar.focusBorder",
+    "titleBar.border",
+    "menubar.selectionBorder",
+    "menu.selectionBorder",
+    "menu.border",
+    "input.border",
+    "inputOption.activeBorder",
+    "terminal.border",
+    "panel.border",
+    "editorGroupHeader.border",
+    "editorGroupHeader.tabsBorder",
+    "tab.border",
+    "tab.activeBorder",
+    "tab.activeBorderTop",
+    "tab.hoverBorder",
+    "pickerGroup.border",
+    "debugToolBar.border",
+    "notificationToast.border",
+    "notifications.border",
+    "list.focusoutline",
+  ]
+
+};
 
 function deactivate() { }
 
